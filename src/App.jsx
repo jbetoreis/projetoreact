d924592aaf400dc41cc06e738d2ddb0fb3fc7a9d
José <josebeto1113@gmail.com>
2024-10-30 22:39:33 -0300

    Feat: Adicionado componente para listar tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+
 function App() {
+  const [tasks, addTask] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      done: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Olá Mundo</h1>
+    <div className="w-screen h-screen bg-slate-400 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-slate-50 text-center text-3xl font-bold">
+          Gerenciador Tarefas
+        </h1>
+        <TaskList />
+        <AddTask />
+      </div>
     </div>
   );
 }
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+function TaskList() {
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+    </div>
+  );
+}
+
+export default TaskList;
