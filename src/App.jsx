45c9985b9078c6d16537990c3dd52423f53ec0ae
José <josebeto1113@gmail.com>
2024-11-02 23:42:02 -0300

    Feat: Inserido função para alterar o estado de conclusão de uma tarefa por meio de states e props

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,38 @@ import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 
 function App() {
-  const [tasks, addTask] = useState([
+  const [tasks, updateTasks] = useState([
     {
       id: 1,
       title: "Estudar React",
       done: false,
     },
+    {
+      id: 2,
+      title: "Estudar .NET",
+      done: false,
+    },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    });
+
+    updateTasks(newTasks);
+  }
+
   return (
-    <div className="w-screen h-screen bg-slate-400 flex justify-center p-6">
+    <div className="w-screen h-screen bg-gray-400 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-slate-50 text-center text-3xl font-bold">
           Gerenciador Tarefas
         </h1>
-        <TaskList />
         <AddTask />
+        <TaskList tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,25 @@
-function TaskList() {
+import { Check, Eye, Square } from "lucide-react";
+
+function TaskList(props) {
   return (
-    <div>
-      <h1>Lista de Tarefas</h1>
-    </div>
+    <ul className="bg-slate-500 p-5 rounded-lg flex flex-col gap-3">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex flex-row gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-300 p-3 text-white text-left rounded-md w-full flex gap-1 ${
+              task.done && "line-through"
+            }`}
+          >
+            {task.done ? <Check /> : <Square />}
+            {task.title}
+          </button>
+          <button className="bg-slate-300 p-3 text-white rounded-md">
+            <Eye />
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
 
